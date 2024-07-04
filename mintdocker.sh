7638d60adfe1d728d451356ee62cfeb600a0750b
dallagrana <dallagrana@gmail.com>
2024-07-04 11:21:50 +0200

    Create mintdocker.sh

---
+++ mintdocker.sh
@@ -0,0 +1,11 @@
+## install docker linuxmint 21.3
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt -y remove docker docker-engine docker.io containerd runc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo usermod -aG docker $USER
+newgrp docker
